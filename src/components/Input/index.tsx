ca7fdb517355f75014b9418e361aeb42dcb8876f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-06 16:32:11 -0300

    core: placeholder style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,8 @@
 import { Container } from "./styles";
 import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
 
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const theme = useTheme();
+  return <Container placeholderTextColor={theme.COLORS.GREY_300} {...rest} />;
 }
