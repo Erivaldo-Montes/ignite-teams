1d7ec742d1e4c7429ab167c9348b56a87b4d0c02
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-08 22:12:43 -0300

    players

--- App.tsx
+++ App.tsx
@@ -1,23 +1,26 @@
-import {ThemeProvider} from 'styled-components'
-import { StatusBar } from 'react-native'
-import {Roboto_400Regular, Roboto_700Bold, useFonts} from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from 'react-native';
-import Theme from  './src/themes'
-import { NewGroup } from '@screens/NewGroup';
-import {Loading} from '@components/Loading'
+import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
+import Theme from "./src/themes";
+import { Players } from "@screens/Players";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  let [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  let [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={Theme}>
       <StatusBar
-        barStyle={'light-content'}
+        barStyle={"light-content"}
         translucent
-        backgroundColor={'transparent'} 
+        backgroundColor={"transparent"}
       />
-     {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
-
