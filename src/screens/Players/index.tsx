c56a5cd413580eb6695d137eb5c8379051f4e363
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-12 13:07:07 -0300

    show players in team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,15 @@
 import { FlatList } from "react-native";
+import { useState } from "react";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+
 import { Highlight } from "@components/Highlight";
 import { Header } from "@components/Header";
 import { Filter } from "@components/Filter";
+import { Button } from "@components/Button";
+import { PlayerCard } from "@components/PlayerCard";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
-import { useState } from "react";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Players() {
   const [team, setTeam] = useState("time a");
@@ -35,11 +39,29 @@ export function Players() {
               onPress={() => setTeam(item)}
             />
           )}
+          showsHorizontalScrollIndicator={false}
           horizontal
         />
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} key={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+      <Button title="Remover turma" type="SECONDARY" />
     </Container>
   );
 }
