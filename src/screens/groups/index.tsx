ffd11cae1b7d1ae20ddf8bbfa3a12a3c3b0b29cb
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-12 16:45:11 -0300

    navtive stack navigation

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,36 +1,50 @@
-import { useState } from 'react';
-import { FlatList } from 'react-native';
-import { Header } from '@components/Header'
-import {GroupCard} from '@components/GroupCard'
-import { Highlight } from '@components/Highlight';
-import { Button } from '@components/Button'
-import {ListEmpty} from '@components/ListEmpty'
-import { Container,  } from './styles'
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { ListEmpty } from "@components/ListEmpty";
+import { Container } from "./styles";
 
-export function Groups() {
-  const [groups, setGroups] = useState([])
+type RootParamList = {
+  new: undefined;
+  group: undefined;
+  player: {
+    group: string;
+  };
+};
 
+type Props = {
+  navigation: NativeStackNavigationProp<RootParamList, "group">;
+};
+
+export function Groups({ navigation }: Props) {
+  const [groups, setGroups] = useState([]);
+
+  function handleNewGroups() {
+    navigation.navigate("new");
+  }
   return (
     <Container>
       <Header />
-      <Highlight 
-        title='Turmas'
-        subtitle='Jogue com sua turma'
-      />
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
 
       <FlatList
         data={groups}
-        keyExtractor={item => item}
-        contentContainerStyle={groups.length === 0 && {flex: 1}}
-        ListEmptyComponent={() => <ListEmpty message="Que tal cadatrar a primeira turma?" />}
-        renderItem={({item}) => (
-          <GroupCard  title={item} />
+        keyExtractor={(item) => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadatrar a primeira turma?" />
         )}
+        renderItem={({ item }) => <GroupCard title={item} />}
       />
 
       <Button
-        type='SECONDARY' 
-        title='Criar nova turma' 
+        type="PRIMARY"
+        title="Criar nova turma"
+        onPress={handleNewGroups}
       />
     </Container>
   );
