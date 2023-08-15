14a43cda0dc94abc446de5c77247ed5d75b7567f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-15 19:47:48 -0300

    safe area view

--- src/screens/groups/styles.ts
+++ src/screens/groups/styles.ts
@@ -1,8 +1,7 @@
-import styled from 'styled-components/native'
-
-export const Container = styled.View`
+import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export const Container = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${props => props.theme.COLORS.GREY_600};
+  background-color: ${(props) => props.theme.COLORS.GREY_600};
   padding: 24px;
-`
-
+`;
