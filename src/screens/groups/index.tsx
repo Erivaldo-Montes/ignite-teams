388c26dd8745093e6628f7c5eadda5e1fbdb145f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-15 19:45:20 -0300

    navigation refactor

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
 import { Highlight } from "@components/Highlight";
@@ -8,24 +9,14 @@ import { Button } from "@components/Button";
 import { ListEmpty } from "@components/ListEmpty";
 import { Container } from "./styles";
 
-type RootParamList = {
-  new: undefined;
-  group: undefined;
-  player: {
-    group: string;
-  };
-};
-
-type Props = {
-  navigation: NativeStackNavigationProp<RootParamList, "group">;
-};
-
-export function Groups({ navigation }: Props) {
+export function Groups() {
   const [groups, setGroups] = useState([]);
+  const navigation = useNavigation();
 
   function handleNewGroups() {
     navigation.navigate("new");
   }
+
   return (
     <Container>
       <Header />
