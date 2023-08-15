a284e640b4e4cdac8edd11fb762144f577bfc345
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-15 19:48:56 -0300

    group name

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { Container, Content, Icon } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  const [nameGroup, setNameGroup] = useState("");
+
+  function handleNew() {
+    navigation.navigate("players", {
+      group: nameGroup,
+    });
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -16,9 +27,9 @@ export function NewGroup() {
           subtitle="Crie uma nova turma para adicionar pessoas"
         />
 
-        <Input placeholder="nome da turma" />
+        <Input placeholder="nome da turma" onChangeText={setNameGroup} />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
