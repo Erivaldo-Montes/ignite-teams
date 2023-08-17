f4f28730a8a957c028be6071a1133af62aa1eb8f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-16 21:55:18 -0300

    open team

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
+import { groupGetAll } from "@storage/group/groupGetAll";
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
 import { Highlight } from "@components/Highlight";
@@ -10,12 +11,31 @@ import { ListEmpty } from "@components/ListEmpty";
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState([]);
+  const [groups, setGroups] = useState<string[]>([]);
   const navigation = useNavigation();
 
   function handleNewGroups() {
     navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
@@ -29,7 +49,9 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadatrar a primeira turma?" />
         )}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
       />
 
       <Button
