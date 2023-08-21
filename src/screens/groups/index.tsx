7a910cd7b9e8e4a65acea4e4f716eeb94ae3938d
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-21 14:48:14 -0300

    loading animation fetch groups

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -9,10 +9,12 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { ListEmpty } from "@components/ListEmpty";
 import { Container } from "./styles";
+import { Loading } from "@components/Loading";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
   const navigation = useNavigation();
+  const [groupIsLoading, setGroupIsLoading] = useState(true);
 
   function handleNewGroups() {
     navigation.navigate("new");
@@ -20,10 +22,13 @@ export function Groups() {
 
   async function fetchGroups() {
     try {
+      setGroupIsLoading(true);
       const data = await groupGetAll();
       setGroups(data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setGroupIsLoading(false);
     }
   }
 
@@ -42,17 +47,21 @@ export function Groups() {
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
 
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty message="Que tal cadatrar a primeira turma?" />
-        )}
-        renderItem={({ item }) => (
-          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
-        )}
-      />
+      {groupIsLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+          ListEmptyComponent={() => (
+            <ListEmpty message="Que tal cadatrar a primeira turma?" />
+          )}
+          renderItem={({ item }) => (
+            <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+          )}
+        />
+      )}
 
       <Button
         type="PRIMARY"
